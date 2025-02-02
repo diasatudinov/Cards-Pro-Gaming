92358417d97d98e96c2da8776d4d31b29ab1a68d
Dias <dias.atudinov@gmail.com>
2025-02-02 20:39:47 +0500

    new files

---
+++ Cards Pro Gaming/Cards Pro Gaming/Cards_Pro_GamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cards_Pro_GamingApp.swift
+//  Cards Pro Gaming
+//
+//  Created by Dias Atudinov on 02.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Cards_Pro_GamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cards Pro Gaming/Cards Pro Gaming/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cards Pro Gaming
+//
+//  Created by Dias Atudinov on 02.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
