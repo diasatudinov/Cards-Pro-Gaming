60d0ef2ffc407167e37ea537e624b8896cf791ac
Dias <dias.atudinov@gmail.com>
2025-02-03 18:13:49 +0500

    Coins background

--- Cards Pro Gaming/Cards Pro Gaming/Helpers/CoinsBg.swift
+++ Cards Pro Gaming/Cards Pro Gaming/Helpers/CoinsBg.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct CoinsBg: View {
+    @State var coins: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 4) {
+            Image(.coinIcon)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 30)
+                .padding(5)
+                .background(
+                    Color.mainGreen
+                )
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.yellow, lineWidth: 1)
+                )
+            Text(coins)
+                .font(.custom(Fonts.bold.rawValue, size: 24))
+                .foregroundStyle(.yellow)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 6)
+                .background(
+                    Color.mainGreen
+                )
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.yellow, lineWidth: 1)
+                )
+        }.frame(width: 200)
     }
 }
 
 #Preview {
-    CoinsBg()
+    CoinsBg(coins: "100")
 }
