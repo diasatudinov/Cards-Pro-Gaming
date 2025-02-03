b8156120e7a44f1feccaed00ee39d177768914e5
Dias <dias.atudinov@gmail.com>
2025-02-03 14:29:18 +0500

    loading view

--- Cards Pro Gaming/Cards Pro Gaming/Fonts/Fonts.swift
+++ Cards Pro Gaming/Cards Pro Gaming/Fonts/Fonts.swift
@@ -1,5 +1,13 @@
+//
+//  Fonts.swift
+//  Cards Pro Gaming
+//
+//  Created by Dias Atudinov on 03.02.2025.
+//
+
+
 import Foundation
 
 enum Fonts: String {
-    case regular = "Koulen-Regular"
+    case bold = "AbhayaLibre-Bold"
 }
--- Cards Pro Gaming/Cards Pro Gaming/Moduls/MainMenu/LoadingView.swift
+++ Cards Pro Gaming/Cards Pro Gaming/Moduls/MainMenu/LoadingView.swift
@@ -8,8 +8,71 @@
 import SwiftUI
 
 struct LoadingView: View {
+    @State private var progress: Double = 0.0
+    @State private var timer: Timer?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                HStack {
+                    Spacer()
+                }
+                Spacer()
+                Image(.logo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                Spacer()
+                ZStack {
+                    VStack {
+                        ZStack {
+                                
+                            ProgressView(value: progress, total: 100)
+                                .progressViewStyle(LinearProgressViewStyle())
+                                .cornerRadius(20)
+                                .accentColor(Color.mainGreen)
+                                .padding(.horizontal, 6)
+                                
+                                .scaleEffect(y: 6.0, anchor: .center)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .stroke(Color.yellow, lineWidth: 1)
+                                        .frame(height: 31)
+                                }
+                                .padding(.horizontal, 6)
+                            
+                            Text("Loading...")
+                                .font(.custom(Fonts.bold.rawValue, size: 16))
+                                .foregroundStyle(.yellow)
+                                .textCase(.uppercase)
+                        }.frame(width: UIScreen.main.bounds.width / 2.5)
+                        
+                    }
+                }
+                .foregroundColor(.black)
+                .padding(.bottom, 25)
+            }
+        }.background(
+            Color.mainRed
+                .ignoresSafeArea()
+                .scaledToFill()
+                
+        )
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 100 {
+                progress += 1
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
 
