5e59403709673134196dd08f1563d914fe0fe5ff
Dias <dias.atudinov@gmail.com>
2025-02-03 14:29:03 +0500

    new files

---
+++ Cards Pro Gaming/Cards Pro Gaming/Moduls/MainMenu/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Cards Pro Gaming
+//
+//  Created by Dias Atudinov on 03.02.2025.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
